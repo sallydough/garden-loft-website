60d577edb9fb48eb16445a84b19478fb4c3a2139
Meseret <mesiteklu2018@gmail.com>
2024-06-03 12:23:32 -0600

    added home and safety componenet

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <section className="home">
+      <div className="home-content">
+        <h1>Garden Loft <br />Backyard Suites</h1>
+        <p>family focused <br />supportive living</p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
