a73724990713d0b68503d81d12b354e56b41467b
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-06-04 10:47:28 -0600

    edit hamburger menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,66 +1,3 @@
-// import React, { useState } from "react";
-// import "./Navbar.css"; // Import your CSS file for styling
-// import { Link } from "react-router-dom";
-// import GLLogo from "../../assets/gloft-logo.svg";
-
-// const Navbar: React.FC = () => {
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setIsMenuOpen(!isMenuOpen);
-//   };
-
-//   return (
-//     <nav className="navbar">
-//       <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-//         <div className="logo">
-//           <img src={GLLogo} alt="Logo" />
-//           <h1>garden loft</h1>
-//         </div>
-//       </Link>
-//       <div className={`menu-web ${isMenuOpen ? "open" : ""}`} id="menu-web">
-//         <p>
-//           <a href="#support-features">Safety Features</a>
-//         </p>
-//         <p>
-//           <a href="#garden-loft-advantage">Garden Loft Advantage</a>
-//         </p>
-//         <p>
-//           <a href="#two-units">Our Units</a>
-//         </p>
-//         <p>
-//           <a href="#compare-costs">Compare Our Costs</a>
-//         </p>
-//         <p>
-//           <a href="#four-step-process">4-Step Process</a>
-//         </p>
-//         <p>
-//           <a href="#rules-and-grants">Rules + Grants</a>
-//         </p>
-//         <p>
-//           <a href="#garden-lofters">Meet Our Garden Lofters</a>
-//         </p>
-//         <Link to="/faq" style={{ textDecoration: "none" }}>
-//           <p>FAQ</p>
-//         </Link>
-//         <Link to="/tour" style={{ textDecoration: "none" }}>
-//         <div className="button1">
-//           <a href="#our-units">Book a Tour</a>
-//         </div>
-//         </Link>
-//       </div>
-//       <div className="hamburger-menu" id="hamburger-menu" onClick={toggleMenu}>
-//         <span className="hamburger"></span>
-//         <span className="hamburger"></span>
-//         <span className="hamburger"></span>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
-
 import React, { useState, useEffect } from "react";
 import "./Navbar.css"; // Import your CSS file for styling
 import { Link } from "react-router-dom";
@@ -135,9 +72,15 @@ const Navbar: React.FC = () => {
         </Link>
       </div>
       <div className="hamburger-menu" id="hamburger-menu" onClick={toggleMenu}>
-        <span className="hamburger"></span>
-        <span className="hamburger"></span>
-        <span className="hamburger"></span>
+        <span  className={`hamburger ${isScrolled ? "scrolled" : ""} ${
+        isNavbarVisible ? "visible" : "hidden"
+      }`}></span>
+        <span className={`hamburger ${isScrolled ? "scrolled" : ""} ${
+        isNavbarVisible ? "visible" : "hidden"
+      }`}></span>
+        <span className={`hamburger ${isScrolled ? "scrolled" : ""} ${
+        isNavbarVisible ? "visible" : "hidden"
+      }`}></span>
       </div>
     </nav>
   );
