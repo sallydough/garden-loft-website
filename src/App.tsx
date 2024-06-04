4f9e3ab3467e8e39e4bfff045389e170021fcc19
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-06-03 18:14:13 -0600

    commit linking and grants

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,35 @@
-import React from 'react'
-import Home from './components/Home/Home'
-import './App.css'
-import Safety from './components/Safety/Safety'
-import Navbar from './components/Navbar/Navbar'
-import Carousel from './components/Carousel';  
-import Units from './components/Units/Units';
-import Costs from './components/Costs/Costs'
-import Footer from './components/Footer/Footer';
-import Costs from './components/Costs/Costs'
-import Grants from './components/Grants/Grants'
-
-import Process from './components/Process/Process'
-
-import Testimony from './components/Testimony/Testimony'
-import Report from './components/Report/Report'
-
+import React from "react";
+import Home from "./components/Home/Home";
+import "./App.css";
+import Safety from "./components/Safety/Safety";
+import Navbar from "./components/Navbar/Navbar";
+import Carousel from "./components/Carousel";
+import Units from "./components/Units/Units";
+import Costs from "./components/Costs/Costs";
+import Footer from "./components/Footer/Footer";
+import Grants from "./components/Grants/Grants";
+import Process from "./components/Process/Process";
+import Testimony from "./components/Testimony/Testimony";
+import Report from "./components/Report/Report";
 
 function App() {
-  
   return (
     <>
       <div>
-       <Navbar />
-       <Home/>
-
-       <Safety/>
-       <Carousel />
-       <Report/>
-       <Units />
-       <Costs />
-       <Grants />
-
-       <Costs />
-       <Process />
-
-       <Testimony/>
-
-       <Footer/>
-
+        <Navbar />
+        <Home />
+        <Safety />
+        <Carousel />
+        <Report />
+        <Units />
+        <Costs />
+        <Process />
+        <Grants />
+        <Testimony />
+        <Footer />
       </div>
-   
-   
     </>
-  )
+  );
 }
 
 export default App;
